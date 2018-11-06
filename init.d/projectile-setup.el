3b650386f8ac9b4097ff2a2a8c098d548ae918ae
Victor Hui <VC-H@users-noreplys.github.com>
2018-11-06 06:08:21 +0000

    configured projectile cache, indexing-method and completion-system:

--- init.d/projectile-setup.el
+++ init.d/projectile-setup.el
@@ -8,12 +8,16 @@
 
 (when (package-installed-p 'projectile)
   (projectile-global-mode)
+  (setq projectile-enable-caching t)
+  (setq projectile-indexing-method 'git)
   (setq projectile-use-git-grep t)
+  (setq projectile-file-exists-remote-cache-expire nil)
   (if (package-installed-p 'counsel) ; installed in init.el
       (unless (package-installed-p 'counsel-projectile)
 	(package-install 'counsel-projectile)))
   (when (package-installed-p 'counsel-projectile)
     (require 'counsel-projectile)
-    (counsel-projectile-mode))
+    (counsel-projectile-mode)
+    (setq projectile-completion-system 'ivy))
   (provide 'projectile-setup)
   )
